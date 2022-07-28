3e995a9b3cdf10bb445bbd6d003adce7859974eb
Emily Mitcheson-Smith <106613906+emmymitch@users.noreply.github.com>
2022-07-28 12:43:57 +0100

    Added check for if you manage to hit the div

--- src/components/TaskCard/TaskCard.jsx
+++ src/components/TaskCard/TaskCard.jsx
@@ -2,21 +2,32 @@ import "./TaskCard.scss";
 import bin from "../../assets/icons/bin-icon.svg";
 import { useState } from "react";
 
-const TaskCard = ({taskText}) => {
+const TaskCard = ({taskText, deleteTask}) => {
     const [taskState, setTaskState] = useState(false);
 
     const taskStateChange = (event) => {
         setTaskState(!taskState)
 
         //Ensure checkbox and strikethrough are simultaneous
-        if (!event.target.parentElement.className.includes("strikethrough")){
-            event.target.previousSibling.checked = true;  
-        } else{
-            event.target.previousSibling.checked = false;
-        };
+        //First if: for if you manage to hit the div ie the small gap between the checkbox and text
+        if (event.target.className.includes("task-card__success-click")){
+            if (!event.target.className.includes("strikethrough")){
+                event.target.firstChild.checked = true;
+            } else if(event.target.className.includes("strikethrough")){
+                event.target.firstChild.checked = false;
+            };    
 
+        //Otherwise just check the box and strikethrough simultaneously
+        } else{
+            if (!event.target.parentElement.className.includes("strikethrough")){
+                event.target.previousSibling.checked = true;  
+            } else{
+                event.target.previousSibling.checked = false;
+            };
+        }
     };
 
+    
     return (
         <div key={taskText} className="task-card">
             <div onClick={taskStateChange} className={"task-card__success-click "+(taskState? "strikethrough" : "")}>
@@ -24,7 +35,7 @@ const TaskCard = ({taskText}) => {
                 <label htmlFor="check" className="task-card__text">{taskText}</label>
             </div>
 
-            <img className="task-card__bin" src={bin} alt="Remove task" />
+            <img onClick={deleteTask} className="task-card__bin" src={bin} alt="Remove task" />
         </div>
     );
 };
