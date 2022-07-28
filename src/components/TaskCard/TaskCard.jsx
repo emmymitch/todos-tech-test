326af046a6494c330cfac68c12f47441fcc6049f
Emily Mitcheson-Smith <106613906+emmymitch@users.noreply.github.com>
2022-07-28 12:19:43 +0100

    Added check/strikethrough on click

--- src/components/TaskCard/TaskCard.jsx
+++ src/components/TaskCard/TaskCard.jsx
@@ -1,12 +1,27 @@
 import "./TaskCard.scss";
 import bin from "../../assets/icons/bin-icon.svg";
+import { useState } from "react";
 
 const TaskCard = ({taskText}) => {
+    const [taskState, setTaskState] = useState(false);
+
+    const taskStateChange = (event) => {
+        setTaskState(!taskState)
+
+        //Ensure checkbox and strikethrough are simultaneous
+        if (!event.target.parentElement.className.includes("strikethrough")){
+            event.target.previousSibling.checked = true;  
+        } else{
+            event.target.previousSibling.checked = false;
+        };
+
+    };
+
     return (
-        <div className="task-card">
-            <div className="task-card__success-click">
-                <input className="task-card__checkbox" type="checkbox" />
-                <p className="task-card__text">{taskText}</p>
+        <div key={taskText} className="task-card">
+            <div onClick={taskStateChange} className={"task-card__success-click "+(taskState? "strikethrough" : "")}>
+                <input name="check" className="task-card__checkbox" type="checkbox" />
+                <label htmlFor="check" className="task-card__text">{taskText}</label>
             </div>
 
             <img className="task-card__bin" src={bin} alt="Remove task" />
