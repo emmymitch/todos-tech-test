4dc023062d3495dd5026e5c4eb6bdaf1db7f2792
Emily Mitcheson-Smith <106613906+emmymitch@users.noreply.github.com>
2022-07-28 11:12:42 +0100

    Added task card & condition to show

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import './App.scss';
 import Header from './components/Header/Header';
+import TaskBar from './components/TaskBar/TaskBar';
+import TaskCard from './components/TaskCard/TaskCard';
 
 const App = () => {
+ let tasks = [1];
+
   return (
     <div className="app">
       <Header />
+      <TaskBar />
+
+      {tasks.length !== 0 
+        ? <TaskCard />
+        : <p className='no-cards-text'>Nothing to see here yet... Add a task in the field above! ☝</p>
+      }
+
     </div>
   );
 }
