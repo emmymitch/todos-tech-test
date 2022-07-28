63e2b5766f64e70b490b6cc749a8320760847965
Emily Mitcheson-Smith <106613906+emmymitch@users.noreply.github.com>
2022-07-28 13:44:39 +0100

    Added default value

--- src/components/TaskBar/TaskBar.jsx
+++ src/components/TaskBar/TaskBar.jsx
@@ -4,7 +4,7 @@ import plus from "../../assets/icons/plus-circle-icon.svg";
 const TaskBar = ({addTask}) => {
     return(
         <div className="taskbar">
-            <input className="taskbar__input" type="text" placeholder="Add your task here..." />
+            <input className="taskbar__input" type="text" defaultValue="" placeholder="Add your task here..." />
             <img onClick={addTask} className="taskbar__img" src={plus} alt="Add task to list" />
         </div>
     )
